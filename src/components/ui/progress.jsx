32a68f15f2d6ca90fe08a23624de7374354803f3
cubewin <tanthang071208@gmail.com>
2025-07-03 16:07:57 +0700

    Fixing not running progress bar

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -7,13 +7,13 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      "relative h-2 w-full overflow-hidden rounded-full bg-base-300",
       className
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full bg-blue-500 transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
